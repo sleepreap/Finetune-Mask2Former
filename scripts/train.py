f47b5a9c29194acd7ba118eeb218578c1792258b
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-04-15 14:25:01 +0800

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -2,14 +2,14 @@ import pytorch_lightning as pl
 import torch
 torch.manual_seed(1)
 torch.set_float32_matmul_precision("medium")
-from segformer.model import SegformerFinetuner
-from segformer.dataset import SegmentationDataModule
-import segformer.config as config
+from mask2former.model import Mask2FormerFinetuner
+from mask2former.dataset import SegmentationDataModule
+import mask2former.config as config
 
 
 if __name__=="__main__":
     data_module = SegmentationDataModule(dataset_dir=config.DATASET_DIR, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS)
-    model=SegformerFinetuner(config.ID2LABEL, config.LEARNING_RATE)
+    model=Mask2FormerFinetuner(config.ID2LABEL, config.LEARNING_RATE)
 
     trainer = pl.Trainer(
         logger=config.LOGGER,
@@ -22,4 +22,4 @@ if __name__=="__main__":
     print("Training starts!!")
     trainer.fit(model,data_module)
     print("saving model!")
-    trainer.save_checkpoint("segformer_checkpoint_hyperparameters.ckpt")
+    trainer.save_checkpoint("mask2former.ckpt")
