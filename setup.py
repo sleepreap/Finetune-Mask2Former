97d79af29a1f98f4cdeae15812bc7babf125df5b
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-04-16 14:48:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name='segformer',
+setup(name='Mask2Former',
       python_requires=">=3.10",
       install_requires=[
           "transformers",
