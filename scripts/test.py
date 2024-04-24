16086832ccc5e93c597d8352db5bcd922024dbac
sleepreap <98008874+sleepreap@users.noreply.github.com>
2024-04-24 12:43:17 +0800

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -2,8 +2,16 @@ import pytorch_lightning as pl
 import torch
 torch.manual_seed(1)
 torch.set_float32_matmul_precision("medium")
-from mask2former.model import Mask2FormerFinetuner
-from mask2former.dataset import SegmentationDataModule
+from mask2former import ( Mask2FormerFinetuner, 
+                        SegmentationDataModule, 
+                        DATASET_DIR, 
+                        BATCH_SIZE, 
+                        NUM_WORKERS, 
+                        ID2LABEL, 
+                        LEARNING_RATE, 
+                        LOGGER, 
+                        PRECISION)
+
 import mask2former.config as config
 import argparse
 
@@ -19,12 +27,12 @@ if __name__=="__main__":
 
     args = parser.parse_args()
     model_path = args.model_path
-    data_module = SegmentationDataModule(dataset_dir=config.DATASET_DIR, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS)
-    model = Mask2FormerFinetuner.load_from_checkpoint(model_path,id2label=config.ID2LABEL, lr=config.LEARNING_RATE)
+    data_module = SegmentationDataModule(dataset_dir=DATASET_DIR, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    model=Mask2FormerFinetuner(ID2LABEL, LEARNING_RATE)
 
     trainer = pl.Trainer(
-        logger=config.LOGGER,
-        precision=config.PRECISION,
+        logger=LOGGER,
+        precision=PRECISION,
         accelerator='cuda',
         devices=[0],
         num_nodes=1,
